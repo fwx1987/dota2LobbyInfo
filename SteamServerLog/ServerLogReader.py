c9527fedd496b1df6db88cba27849750b40ada66
fwx1987 <wenxiang.feng@gmail.com>
2018-12-04 20:50:29 +0800

    update logreader with registry reading capability

--- SteamServerLog/ServerLogReader.py
+++ SteamServerLog/ServerLogReader.py
@@ -1,6 +1,6 @@
 import os,socket
 import time,logging,logging.handlers
-import config
+import config,sys
 
 config.get_root_directory()
 
@@ -62,6 +62,40 @@ class LatestLobby:
 
 
 def get_server_log_file():
+
+    if sys.platform == "win32":
+        pass
+    #HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 570
+        import errno, os, winreg
+        proc_arch = os.environ['PROCESSOR_ARCHITECTURE'].lower()
+
+        if proc_arch == 'x86' or proc_arch == 'amd64':
+            arch_keys = {winreg.KEY_WOW64_32KEY, winreg.KEY_WOW64_64KEY}
+        else:
+            raise Exception("Unhandled arch: %s" % proc_arch)
+
+        for arch_key in arch_keys:
+            key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", 0, winreg.KEY_READ | arch_key)
+            for i in range(0, winreg.QueryInfoKey(key)[0]):
+                skey_name = winreg.EnumKey(key, i)
+                skey = winreg.OpenKey(key, skey_name)
+                try:
+                    #print(winreg.QueryValueEx(skey, 'DisplayName')[0])
+                    #print(winreg.QueryValueEx(skey, 'InstallLocation')[0])
+                    if winreg.QueryValueEx(skey, 'DisplayName')[0] == "Dota 2":
+                        log_file = winreg.QueryValueEx(skey, 'InstallLocation')[0] +"\\game\\dota\\server_log.txt"
+                        logger.debug("get dota2 log file at :"+log_file)
+                        return open(log_file,'r')
+
+                except OSError as e:
+                    if e.errno == errno.ENOENT:
+                        # DisplayName doesn't exist in this skey
+                        pass
+                finally:
+                    skey.Close()
+        return None
+    else:
+        raise Exception ("Unknown platform detected:"+str(sys.platform))
     if socket.gethostname() == "DESKTOP-Wenxiang":
         f1 = 'C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\server_log.txt'
         f2 = 'C:\Program Files (x86)\Steam\steamapps\common\dota 2 beta\game\dota\server_log.txt'
